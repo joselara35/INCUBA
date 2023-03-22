bdb4e607ef66b5fb0f3eeac08b6a8f812c75c206
Jose Lara <josecarloslara35@gmail.com>
2023-03-22 17:52:19 -0400

    Actualizacion pantallaPrincipal

--- app/src/main/java/com/incuba/app/Login.kt
+++ app/src/main/java/com/incuba/app/Login.kt
@@ -1,11 +1,22 @@
 package com.incuba.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.incuba.app.databinding.ActivityLoginBinding
+import com.incuba.app.databinding.ActivitySplashScreenBinding
 
 class Login : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //--------------------------------------------------------------
+        binding.btnEntrar.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
